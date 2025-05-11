4129e1de44af4d159b2426ad43dad482b0f641b4
Pascal Adrian <aa4033339@icloud.com>
2025-05-11 22:49:47 +0300

    feat: add https support for the http request function

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import socket
+import ssl
 
-
-def send_http_request(host, port, request, timeout=15):
+def send_http_request(host, port, request, is_https=True, timeout=15):
     """
     Send an HTTP request and return the response.
     :param host: Hostname or IP address of the server
     :param port: Port number of the server
     :param request: HTTP request string
+    :param is_https: Boolean indicating if the request is HTTPS
     :param timeout: Timeout for the connection in seconds
     """
 
@@ -15,6 +16,12 @@ def send_http_request(host, port, request, timeout=15):
     try:
         sock.settimeout(timeout)
         sock.connect((host, port))
+
+        if is_https:
+            context = ssl.create_default_context()
+            context.check_hostname = False
+            context.verify_mode = ssl.CERT_NONE
+            sock = context.wrap_socket(sock, server_hostname=host)
 
         sock.sendall(request.encode())
 
@@ -48,7 +55,7 @@ def send_http_request(host, port, request, timeout=15):
 if __name__ == "__main__":
     host = "jsonplaceholder.typicode.com"
     port = 443
-    request = "GET /todos/1 HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n"
+    request = "GET /todos HTTP/1.1\r\nHost: jsonplaceholder.typicode.com\r\nConnection: close\r\n\r\n"
 
     res = send_http_request(host, port, request)
     print(res)
